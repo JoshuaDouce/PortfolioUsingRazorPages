68e35061262e3bec5c8fa214a010c9eb7392f856
Joshua Douce <joshuadouce@gmail.com>
2019-07-05 15:55:47 +0100

    Project detail url handles request for non existent item

--- PortfolioWithRazorPages/Pages/Projects/Detail.cshtml.cs
+++ PortfolioWithRazorPages/Pages/Projects/Detail.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioWithRazorPages.Services;
 
 namespace PortfolioWithRazorPages.Pages.Projects
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private IProjectsService ProjectsService;
+
+        public DetailModel(IProjectsService projectsService)
         {
+            ProjectsService = projectsService;
+        }
+        public async Task<IActionResult> OnGet(long id)
+        {
+            var x = await ProjectsService.FindAsync(id);
+
+            if (x != null)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Error");
         }
     }
 }
