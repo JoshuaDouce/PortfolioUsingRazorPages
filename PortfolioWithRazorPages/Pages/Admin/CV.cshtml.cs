4fac44df45ff47019a08cc3a9ab546dc557b4313
JoshuaDouce <Joshuadouce@gmail.com>
2019-07-22 20:11:14 +0100

    Page stubs and links added for all admin section

---
+++ PortfolioWithRazorPages/Pages/Admin/CV.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioWithRazorPages.Pages.Admin
+{
+    public class CVModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
